2681ae45a4e6af2f1ccf35b8fb4617265323d115
Ricardo_Velez <2000rvelez@gmail.com>
2026-01-22 11:02:56 -0500

    bump node version for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
     domains: [],
   },
-  webpack: (config, {isServer}) => {
+  webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
